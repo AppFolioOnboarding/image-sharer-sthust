2d33e24f54798e2c2adf29df893f76d8c6d77429
Soeren Thust <soeren.thust@appfolio.com>
2020-01-20 13:59:28 -0800

    add link to upload image, upload page, view page, new labels, controller and tests

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -4,6 +4,7 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
   def test_should_get_index
     get root_path
     assert_response :success
-    assert_select '#title', I18n.t(:hello)
+    assert_select '#link_to_image_upload_form', I18n.t(:link_to_image_upload_form)
+    assert_select '#link_to_image_upload_form[href=?]', new_images_path
   end
 end
