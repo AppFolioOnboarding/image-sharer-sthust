692d6b82258abb7ce6fcd20ceec501cb7ae46689
Soeren Thust <soeren.thust@appfolio.com>
2020-01-22 16:35:17 -0800

    st - add tagging functionality and tests to image model

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -5,4 +5,14 @@ class ImageTest < ActiveSupport::TestCase
     invalid_image = Image.new
     refute invalid_image.valid?
   end
+
+  test 'tags can be added/retrieved' do
+    image_with_tag = Image.new(url: 'test1', tag_list: 'tag1')
+    assert_equal ['tag1'], image_with_tag.tag_list
+  end
+
+  test 'tags can be empty' do
+    image_with_empty_tag = Image.new(url: 'test1')
+    assert_equal [], image_with_empty_tag.tag_list
+  end
 end
